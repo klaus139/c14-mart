3c9d1883a290bda1cc7b868f59c87d2eb0702c8a
klausdev <nickoklaus5@gmail.com>
2025-02-22 20:25:02 +0100

    category api completed

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ import express from "express"
 import dotenv from "dotenv"
 dotenv.config();
 
+import categoryRoutes from "./routes/categoryRoutes.js"
+import connectDB from "./config/db.js";
+
 const app = express()
+
+
 
 const port = process.env.PORT || 4000;
 
 //TODO configure database
+connectDB();
 
 
 //middleware
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
+
+//routes
+app.use('/api/category', categoryRoutes)
 
 
 
